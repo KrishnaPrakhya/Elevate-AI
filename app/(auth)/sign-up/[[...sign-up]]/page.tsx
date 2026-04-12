c6a2d4ac6e04ff92ddbb041d17c19366e595a461
KrishnaPrakhya <krishnasaiprakhya@gmail.com>
2026-04-12 12:01:18 +0530

    changes

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 function Page() {
-  return <SignUp />;
+  return <SignUp fallbackRedirectUrl="/onboarding" />;
 }
 
 export default Page;
